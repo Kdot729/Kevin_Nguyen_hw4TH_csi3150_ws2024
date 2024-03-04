033d68da151502ad5e4b1127aac76d80b9a51826
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-03-04 10:10:16 -0500

    Created structure of car models in dom
    Using map to structure the DOM for car models

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
 import usedCars from "./usedCars.js";
 
+usedCars.map(Iterate_Car_Models)
+
+
+function Iterate_Car_Models(Car_Model)
+{
+    let Car_Model_Element = document.createElement("section");
+    document.getElementById("showcase-cars").appendChild(Car_Model_Element);
+};
+
+
