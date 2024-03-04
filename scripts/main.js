7b5071dad676a529e31896eabc6686bbfae3985f
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-03-04 15:29:08 -0500

    Updated imports

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import usedCars from "./usedCars.js";
-import { Setup_Car_Grid, Iterate_Car_Models, Setup_Button_Grid } from "./DOM/elements.js";
+import { Setup_Car_Grid, Iterate_Car_Models } from "./DOM/elements.js";
+import { Setup_Button_Grid } from "./DOM/buttons.js";
 
 Setup_Button_Grid();
 Setup_Car_Grid();
