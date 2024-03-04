3473558dbb9dbd9a55627f7520b3b1c3880cdb76
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-03-04 15:01:36 -0500

    Calling button grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import usedCars from "./usedCars.js";
-import { Setup_Grid, Iterate_Car_Models } from "./elements.js";
+import { Setup_Grid, Iterate_Car_Models, Setup_Button_Grid } from "./elements.js";
 
 Setup_Grid();
+Setup_Button_Grid();
 usedCars.map(Iterate_Car_Models);
 
