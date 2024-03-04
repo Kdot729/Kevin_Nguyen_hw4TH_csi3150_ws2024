af321e6c30638c3985a25cf4dc7ed8ced5607a40
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-03-04 15:27:49 -0500

    Deleted button related elements
    Going to move those elements to a different file

--- scripts/DOM/elements.js
+++ scripts/DOM/elements.js
@@ -1,23 +1,5 @@
 const Root_Div = "showcase-cars";
 
-export function Setup_Button_Grid()
-{
-    let Button_Container = document.getElementById("filter-buttons");
-    let Column_Size = innerWidth / 5;
-    Button_Container.style.cssText =  `
-                                    display: grid; 
-                                    box-sizing: border-box;
-                                    grid-row: 1 / span 1;
-                                    grid-template-columns: repeat(5, ${Column_Size}px);
-                                    grid-template-rows: repeat(1, 100%);
-                                    `;
-};
-
-export function Append_Buttons()
-{
-    let Button_IDs = ["year-button", "make-button", "model-button", "mileage-button", "price-button", "color-button"];
-};
-
 export function Setup_Car_Grid()
 {
     let Grid_Container = document.getElementById(Root_Div);
