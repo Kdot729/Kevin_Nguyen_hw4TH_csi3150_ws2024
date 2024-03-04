f8a95d2c65c6a7f0965fb4fdd5142a71870056ea
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-03-04 11:35:21 -0500

    Changed variable name. Added classname for each element

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,11 @@ usedCars.map(Iterate_Car_Models)
 
 function Iterate_Car_Models(Car_Model)
 {
-    let Car_Model_Element = document.createElement("section");
-    document.getElementById("showcase-cars").appendChild(Car_Model_Element);
+    let Car_Element = document.createElement("section");
+    let Car_Model_With_Dash = Car_Model["model"].replace(/ /g,"-");
+    let Class_Name = `${Car_Model["make"]}-${Car_Model_With_Dash}-${Car_Model["year"]}`;
+    Car_Element.classList.add(Class_Name);
+    document.getElementById("showcase-cars").appendChild(Car_Element);
 };
 
 
