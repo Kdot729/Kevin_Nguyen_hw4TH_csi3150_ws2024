81c28e8c2f186c85b1996bfc9d230a63c0bdc8c5
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-03-05 11:59:06 -0500

    Created loop to create dictionary
    We'll use that dictionary to check the data in usedCars and return whatever matches

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,9 +1,20 @@
 import usedCars from "./usedCars.js";
 
-export function Filter_By_Parameters(Selected_Year)
+export function Filter_By_Parameters(Filter_Parameters)
 {
-    let Year_Car =  usedCars.filter((used_car) =>
+    let Filter_Dictionary = {};
+    for (let Index = 0; Index < Filter_Parameters.length; Index++)
     {
-        return used_car["year"] > Selected_Year;
-    });
+        let Key_and_Value = Filter_Parameters[Index].split("-")
+        Filter_Dictionary[Key_and_Value[0]] = Key_and_Value[1]
+    }
+
+
+    console.log(Filter_Dictionary)
+    // let Filtered_Data =  usedCars.filter((used_car) =>
+    // {
+    //     return used_car.includes(Filter_Parameters);
+    // });
+
+    // console.log(Filtered_Data);
 };
