1369f9ca903e63c217031f4aeff986c1b5d46eed
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-03-05 12:06:29 -0500

    Changed loop
    We'll have a array storing the key and value we want to check in usedCars

--- scripts/filter.js
+++ scripts/filter.js
@@ -2,15 +2,18 @@ import usedCars from "./usedCars.js";
 
 export function Filter_By_Parameters(Filter_Parameters)
 {
-    let Filter_Dictionary = {};
+    console.log(Filter_Parameters)
+    let Array_of_Dictionaries = [];
     for (let Index = 0; Index < Filter_Parameters.length; Index++)
     {
-        let Key_and_Value = Filter_Parameters[Index].split("-")
-        Filter_Dictionary[Key_and_Value[0]] = Key_and_Value[1]
-    }
+        let Emtpy_Dictionary = {};
+        let Key_and_Value = Filter_Parameters[Index].split("-");
+        Emtpy_Dictionary[Key_and_Value[0]] = Key_and_Value[1]
+        Array_of_Dictionaries.push(Emtpy_Dictionary);
+    };
 
 
-    console.log(Filter_Dictionary)
+    console.log(Array_of_Dictionaries)
     // let Filtered_Data =  usedCars.filter((used_car) =>
     // {
     //     return used_car.includes(Filter_Parameters);
