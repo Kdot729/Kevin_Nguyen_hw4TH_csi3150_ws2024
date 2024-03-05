14efe3c4f920ecdb8554e06ba72b5a62cc4130f0
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-03-05 09:09:53 -0500

    Changed the way the loop worked

--- scripts/utility.js
+++ scripts/utility.js
@@ -5,15 +5,15 @@ export function Capitalize_First_Letter(Word)
 
 export function Get_Array_of_Values(usedCars, Index_Dictionary)
 {
+    //Note Because the way the array Filter_By is set up we're doing it this way
+    let Key_for_Dictionary = Index_Dictionary.split("-")[1] ? Index_Dictionary.split("-")[1] : Index_Dictionary.split("-")[0];
     let Hold_Values = []
     for (let Data_Index = 0; Data_Index < usedCars.length; Data_Index++)
     {
-        if (!Hold_Values.includes(usedCars[Data_Index][Index_Dictionary]))
+        if (!Hold_Values.includes(usedCars[Data_Index][Key_for_Dictionary]))
         {
-            Hold_Values.push(usedCars[Data_Index][Index_Dictionary])
+            Hold_Values.push(usedCars[Data_Index][Key_for_Dictionary])
         };
     };
-
     return Hold_Values
-
-}
+};
