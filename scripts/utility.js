f921ba5c7918c1192bf91f9a681949de6ebaf443
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-03-05 07:48:21 -0500

    Created loop to get distinct values in dataset

--- scripts/utility.js
+++ scripts/utility.js
@@ -2,3 +2,18 @@ export function Capitalize_First_Letter(Word)
 {
     return Word.charAt(0).toUpperCase() + Word.slice(1);
 };
+
+export function Get_Array_of_Values(usedCars, Index_Dictionary)
+{
+    let Hold_Values = []
+    for (let Data_Index = 0; Data_Index < usedCars.length; Data_Index++)
+    {
+        if (!Hold_Values.includes(usedCars[Data_Index][Index_Dictionary]))
+        {
+            Hold_Values.push(usedCars[Data_Index][Index_Dictionary])
+        };
+    };
+
+    return Hold_Values
+
+}
